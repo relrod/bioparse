af9b3e5aca75243de92e6483879c160975d4eec0
Ricky Elrod <ricky@elrod.me>
2014-11-28 17:21:58 -0500

    Start of a parser, but ugly
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/Bio/Parse/Sequence/Fasta.hs
+++ src/Bio/Parse/Sequence/Fasta.hs
@@ -1,25 +1,46 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE TemplateHaskell #-}
 module Bio.Parse.Sequence.Fasta where
 
 import Bio.Core.Sequence
+import Control.Applicative
 import Control.Lens
 import qualified Data.ByteString.Lazy.Char8 as BL
+import Data.Monoid (mempty)
+import Data.Typeable
 import Text.Trifecta
 
 data FastaSequence = FastaSequence {
-    _id     :: BL.ByteString
-  , _header :: BL.ByteString
+    _header :: [BL.ByteString]
   , _data   :: BL.ByteString
-  } deriving (Eq, Ord, Show)
+  } deriving (Eq, Ord, Show, Typeable)
 
 makeLenses ''FastaSequence
 
 instance BioSeq FastaSequence where
-  seqid     = SeqLabel . _id
-  seqheader = SeqLabel . _header
+  seqid     = SeqLabel . head . _header
+  seqheader = SeqLabel . BL.concat . _header
   seqdata   = SeqData . _data
   seqlength = fromIntegral . BL.length . _data
 
---parseHeader :: Parser BL.ByteString
---parseHeader = do
---  error "TODO"
+parseHeader :: Parser [BL.ByteString]
+parseHeader = do
+  _ <- char '>'
+  (fmap BL.pack) <$> manyTill anyChar newline `sepBy1` char '|'
+
+parseNucleotides :: Parser BL.ByteString
+parseNucleotides = do
+  nucleotides <- many . choice $ [letter, char '*', char '-', newline]
+  return . BL.pack $ nucleotides
+
+parseSequence :: Parser FastaSequence
+parseSequence = do
+  h <- parseHeader
+  nucleotides <- parseNucleotides
+  return (FastaSequence h nucleotides)
+
+parseSequences :: Parser [FastaSequence]
+parseSequences = manyTill parseSequence eof
+
+parseFasta :: String -> Result [FastaSequence]
+parseFasta = parseString (parseSequences) mempty
