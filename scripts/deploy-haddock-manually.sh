7afdf5f0e5b96e4919747664abad119274244911
Ricky Elrod <ricky@elrod.me>
2014-11-28 05:45:24 -0500

    github haddock deploy script
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

---
+++ scripts/deploy-haddock-manually.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+cwd="$( cd "${BASH_SOURCE[0]%/*}" && pwd )"
+cd "$cwd/.."
+f=`mktemp -d`
+git clone git@github.com:relrod/bioparse.git "$f/bioparse.git"
+cabal haddock
+pushd "$f/bioparse.git"
+  git checkout gh-pages && git rm -rf *
+popd
+mv dist/doc/html/bioparse/* "$f/bioparse.git/"
+pushd "$f/bioparse.git"
+  git add -A
+  git commit -m "Manual docs deploy."
+  git push origin gh-pages
+popd
+rm -rf "$f"
+
+if [ $? == 0 ]; then
+  echo "*** Done: http://relrod.github.io/bioparse/"
+  exit 0
+else
+  echo "*** ERROR!!! Fix the above and try again."
+  exit 1
+fi
