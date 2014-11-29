eb0aa23d2799e333cfffc561844e9933394904d3
Ricky Elrod <ricky@elrod.me>
2014-11-29 18:08:00 -0500

    add a simple (horrible) test
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

---
+++ test/Test.hs
@@ -0,0 +1,28 @@
+module Main where
+
+import qualified Bio.Parse.Sequence.Fasta as Fasta
+import Test.Tasty
+import Test.Tasty.HUnit
+import Text.Trifecta
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [fastaUnitTests]
+
+fastaTest = ">KM034562v1\
+             \CGGACACACAAAAAGAAAGAAGAATTTTTAGGATCTTTTGTGTGCGAATA\n\
+             \ACTATGAGGAAGATTAATAATTTTCCTCTCATTGAAATTTATATCGGAAT\n\
+             \TTAAATTGAAATTGTTACTGTAATCATACCTGGTTTGTTTCAGAGCCATA\n\
+             \TCACCAAGATAGAGAACAACCTAGGTCTCCGGAGGGGGCAAGGGCATCAG\n"
+
+
+fastaUnitTests = testGroup "FASTA Unit tests"
+  [ testCase "Parses a small FASTA file successfully" $
+      successfulParse (Fasta.parseFasta fastaTest)
+  ]
+
+successfulParse :: Result [Fasta.FastaSequence] -> Assertion
+successfulParse (Success _) = return ()
+successfulParse (Failure f) = assertFailure $ "Unsuccessful parse: " ++ show f
