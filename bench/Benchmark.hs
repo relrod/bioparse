1ca3aa14ddde845b09a4463384253b4fae1fbcb3
Ricky Elrod <ricky@elrod.me>
2014-11-29 22:48:49 -0500

    add biofastq benchmarks
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- bench/Benchmark.hs
+++ bench/Benchmark.hs
@@ -1,20 +1,24 @@
 module Main where
 
 import qualified Bio.Parse.Sequence.Fasta as Fasta
+import qualified Bio.Parse.Sequence.FastQ as FastQ
 import qualified Bio.Sequence.Fasta as Biofasta
+import qualified Bio.Sequence.FastQ as BiofastQ
 import Criterion.Main
 import qualified Data.ByteString.Char8 as B
 
-setupEnv :: IO (String, B.ByteString)
+setupEnv :: IO (String, B.ByteString, String, B.ByteString)
 setupEnv = do
   ebola's <- readFile "bench/ebola.fa"
   ebola'bs <- B.readFile "bench/ebola.fa"
-  return (ebola's, ebola'bs)
+  testfq's <- readFile "bench/test.fq"
+  testfq'bs <- B.readFile "bench/test.fq"
+  return (ebola's, ebola'bs, testfq's, testfq'bs)
 
 main :: IO ()
 main =
   defaultMain [
-    env setupEnv $ \ ~(ebola's, ebola'bs) ->
+    env setupEnv $ \ ~(ebola's, ebola'bs, _, _) ->
       bgroup "FASTA/bioparse/parseFasta"
       [
         bench "Bio.Parse.Sequence.Fasta.parseFasta" $ whnf Fasta.parseFasta ebola's
@@ -24,5 +28,14 @@ main =
      [
        bench "Bio.Sequence.Fasta.readFasta" $ whnf Biofasta.readFasta "bench/ebola.fa"
      ]
-
+  , env setupEnv $ \ ~(_, _, testfq's, testfq'bs) ->
+      bgroup "FASTQ/bioparse"
+     [
+        bench "Bio.Parse.Sequence.FastQ.parseFastQ" $ whnf FastQ.parseFastQ testfq's
+      , bench "Bio.Parse.Sequence.FastQ.parseFastaQB" $ whnf FastQ.parseFastQB testfq'bs
+     ]
+  , bgroup "FASTQ/biofastq"
+    [
+       bench "Bio.Sequence.FastQ.readSangerQ" $ whnf BiofastQ.readSangerQ "bench/test.fq"
+     ]
   ]
