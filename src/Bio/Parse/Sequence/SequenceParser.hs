65bfe0360bb9f4965daa8e3d060a33e08c81d24e
Ricky Elrod <ricky@elrod.me>
2017-11-13 07:23:23 -0500

    I can grammar [ci skip]
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/Bio/Parse/Sequence/SequenceParser.hs
+++ src/Bio/Parse/Sequence/SequenceParser.hs
@@ -9,9 +9,9 @@ type ParseConstraint m = (Monad m, CharParsing m, TokenParsing m)
 -- | A general way to talk about \"sequences which can be parsed\".
 --
 -- In a previous life, this talked about a specific parsing function, binding us
--- to a specific parser combinator library, making our rendering our use of
--- @parsers@ pointless. Now we solely rely on 'CharParsing' (from @parsers@)
--- and leave what to do with that fact up to the call site.
+-- to a specific parser combinator library, rendering our use of @parsers@
+-- pointless. Now we solely rely on 'CharParsing' (from @parsers@) and leave
+-- what to do with that fact up to the call site.
 class SequenceParser a where
   parseSequence :: ParseConstraint m => m a
   parseSequences :: ParseConstraint m => m [a]
